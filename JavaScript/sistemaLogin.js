fb7c004fc9161bc249f658655127b910b72aa554
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2026-01-07 13:54:08 -0300

    up

---
+++ JavaScript/sistemaLogin.js
@@ -0,0 +1,8 @@
+function criarConta() {
+    const email = document.getElementById('ilogin').value;
+    const senha = document.getElementById('isenha').value;
+
+    if (!email || !senha) {
+        a
+    }
+}
