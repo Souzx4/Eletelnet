0e3596eeebc5d2e3067962120e1de672f464c16c
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2026-01-06 23:00:58 -0300

    up

---
+++ JavaScript/esqueci.js
@@ -0,0 +1,9 @@
+let codigoGerado = 0; //variavel global para armazenar o dogigo gerado
+
+let emailAlvo = ""; // o email que estamos recuperando a senha
+
+function enviarCodigo(){
+    const email = document.getElementById("emailRecuperacao").value;
+
+    
+}
